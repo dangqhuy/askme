5efd6b91f6f3f9f88462ed5a123e161be18ae52e
dangqhuy <dangqhuy2411@gmail.com>
2018-05-28 16:44:09 +0700

    Done send answer function and log out

--- asking/views.py
+++ asking/views.py
@@ -5,10 +5,12 @@ from django.contrib.auth.models import User
 from asking.models import Question
 from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
 
+
 def ask(request, username):
-    user = User.objects.get(username = username)
+    user = User.objects.get(username=username)
     template = loader.get_template('asking/asking.html')
 
     check_login = True
@@ -16,19 +18,19 @@ def ask(request, username):
         check_login = True
     else:
         check_login = False
+
+        
+
     if not check_login:
         try:
-   
             content_field = request.POST['content']
-            if content_field is not None:
-                user.questions.create(content = content_field, answer = "")
-        
-            return HttpResponseRedirect(reverse('asking:asking', args = (username, )))
+            user.questions.create(content= content_field, answer="")
+            return HttpResponseRedirect(reverse('asking:asking', args=(username, )))
 
-        except:
+        except BaseException:
             try:
-                question = user.questions.filter(answerer_id = user.id).all()
-            except:
+                question = user.questions.filter(answerer_id=user.id).all()
+            except BaseException:
                 pass
 
             first_name = user.first_name
@@ -40,20 +42,29 @@ def ask(request, username):
                 'first_name': first_name,
                 'last_name': last_name,
                 'check_login': check_login,
-                }, request))
+            }, request))
     else:
         try:
-            question_id = request.POST['question_id']
-            answer_field = request.POST['answer']
-            answer = user.questions.get(pk = question_id)
-            answer.answer = answer_field
-            answer.save()
-            return HttpResponseRedirect(reverse('asking:asking', args = (username, )))
-
-        except:
             try:
-                question = user.questions.filter(answerer_id = user.id).all()
+                if request.POST['check_logout'] == "logout":
+                    auth_logout(request)
+                    return HttpResponseRedirect(reverse("login:login"))
             except:
+
+                question_id = request.POST['question_id']
+                answer_field = request.POST['answer']
+                answer = user.questions.get(pk=question_id)
+                answer.answer = answer_field
+                answer.save()
+            
+
+                return HttpResponseRedirect(
+                    reverse('asking:asking', args=(username, )))
+
+        except BaseException:
+            try:
+                question = user.questions.filter(answerer_id=user.id).all()
+            except BaseException:
                 pass
 
             first_name = user.first_name
@@ -65,13 +76,5 @@ def ask(request, username):
                 'first_name': first_name,
                 'last_name': last_name,
                 'check_login': check_login,
-                }, request))
+            }, request))
 
-
-        
-        
-
-   
-   
-   
-    
