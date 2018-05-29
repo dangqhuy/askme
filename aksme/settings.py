89a4932d6c2a38c6dbd5c4e797b8a2c995e7858d
vulinhchi <vulinhchi.05@gmail.com>
2018-05-29 14:21:08 +0700

    template ok

--- aksme/settings.py
+++ aksme/settings.py
@@ -104,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -122,6 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/abc/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    
+]
 
 #LOGOUT_REDIRECT_URL = 'authentication'
