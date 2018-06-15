d4d2661b09d977104702e308d78d9f66e7862db2
dangqhuy <dangqhuy2411@gmail.com>
2018-06-15 16:10:24 +0700

    askmeV2

--- asking/views.py
+++ asking/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.models import User
 
+
 class AskView(CreateView):
     template_name = "asking/asking.html"
     model = Question
